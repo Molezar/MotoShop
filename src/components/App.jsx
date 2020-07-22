5d63d0497b4d12d92bf017173b82bb366862a9eb
Molezar <61122341+Molezar@users.noreply.github.com>
2020-07-23 02:57:53 +0300

    predeploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import Menu from '../containers/Menu';
 class App extends Component {
   componentWillMount() {
     const { setBooks } = this.props;
-    axios.get('/books.json').then(({ data }) => {
+    axios.get('/MotoShop/books.json').then(({ data }) => {
           console.log(data);
       setBooks(data);
     });
